b821fc2cc81e8d64859735bdaf03e4202a8e67e8
Tomáš Kárník <49093071+TomasKarnik@users.noreply.github.com>
2021-06-17 09:10:44 +0200

    Login systém
    Added login systém
    fixed everything

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'byfive');
+define('DB_PASSWORD', 'test.test1337');
+define('DB_NAME', 'notifikacka');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
