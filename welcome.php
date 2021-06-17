104d7708adeb227661b819e9dccedabeb0f5089b
Tomáš Kárník <49093071+TomasKarnik@users.noreply.github.com>
2021-06-17 09:43:28 +0200

    Fixing stuff
    style.css fix

---
+++ welcome.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <div class="grid-container">
+        <div class="grid-item">
+            <a href="logout.php">
+                <button>logout</button>
+             </a>
+        </div>
+        <div class="grid-item">
+            <a href="reset-password.php">
+                <button>resetPW</button>
+             </a>
+        </div>
+    </div>
+</body>
+</html>
